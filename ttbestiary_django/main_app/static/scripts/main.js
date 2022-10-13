e3481e3e1ab4e2f646933a762b2e70b7e4a4d40a
badMeme <noveladdress@gmail.com>
2022-10-13 14:59:13 -0500

    blocking out bottom nav-bar for tools adding basic tools to scripts/main.js

--- ttbestiary_django/main_app/static/scripts/main.js
+++ ttbestiary_django/main_app/static/scripts/main.js
@@ -3,3 +3,13 @@ $(".navbar-burger").click(function () {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
+
+$(".navbar-burger-bottom").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger-bottom").toggleClass("is-active");
+    $(".navbar-menu-bottom").toggleClass("is-active");
+  });
+
+$(".rollable").click(function () {
+  console.log(Math.floor(Math.random()*20)+1)
+})
